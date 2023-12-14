d2e84be63c5c4e55e2d844a5e435cb3ffa8f4959
Leow Fu Lin <fulin.leow@gmail.com>
2023-12-14 12:48:29 +0800

    init

---
+++ src/main/java/app/lunchgowhere/LunchgowhereApplication.java
@@ -0,0 +1,13 @@
+package app.lunchgowhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchgowhereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchgowhereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/lunchgowhere/LunchgowhereApplicationTests.java
@@ -0,0 +1,13 @@
+package app.lunchgowhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchgowhereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
