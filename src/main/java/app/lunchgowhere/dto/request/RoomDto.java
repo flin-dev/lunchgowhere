3b8b83357ec12a875be339c38c1ff0204e5bc58d
Leow Fu Lin <fulin.leow@gmail.com>
2023-12-18 01:20:08 +0800

    fix import and dto structure

---
+++ src/main/java/app/lunchgowhere/dto/request/RoomDto.java
@@ -0,0 +1,13 @@
+package app.lunchgowhere.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RoomDto {
+
+    public String name;
+
+    public String targetTime;
+
+    public String description;
+}
