dfa115660c447574128df109b92d28800137c17a
Leow Fu Lin <fulin.leow@gmail.com>
2023-12-22 01:49:15 +0800

    fix setting for docker compose

--- src/main/java/app/lunchgowhere/LunchgowhereApplication.java
+++ src/main/java/app/lunchgowhere/LunchgowhereApplication.java
@@ -3,11 +3,9 @@ package app.lunchgowhere;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class LunchgowhereApplication {
@@ -29,7 +27,6 @@ public class LunchgowhereApplication {
             }
         };
     }
-
 
     @Bean
     public CommonsRequestLoggingFilter requestLoggingFilter() {
